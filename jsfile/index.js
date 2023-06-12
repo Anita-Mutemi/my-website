13b828a4f07d4a35630d3fdc5bd0a906ba4d48b5
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-13 01:03:30 +0300

    trying to edit the button

--- jsfile/index.js
+++ jsfile/index.js
@@ -2,3 +2,8 @@ document.querySelector('.phone-link').addEventListener('click', function(event) 
     event.preventDefault();
     window.location.href = 'https://www.truecaller.com/';
 });
+
+function sendEmail(email) {
+    var encodedEmail = encodeURIComponent(email);
+    window.location.href = "mailto:" + encodedEmail;
+  }
